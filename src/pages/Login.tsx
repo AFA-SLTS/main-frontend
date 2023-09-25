ca0c558c06eb653c80a98a1dc2b61cc9624b6c94
Adrian <elrubiurc@gmail.com>
2023-09-25 19:51:20 +0200

    feat: build

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
 
 const Login: React.FC=()=>{
-    const [username, setUsername] = useState<string>();
     return(
         <div className="flex h-screen w-full items-center justify-center bg-gray-900 bg-cover bg-no-repeat" >
             <div className="rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
